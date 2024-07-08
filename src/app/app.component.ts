a01cbe23ca61a6158c45d7f1ad2aa93c0af080c9
Cauan Cem <cacocembritosoares@gmail.com>
2024-07-08 16:57:27 -0300

    Atualização Page Content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,14 @@ import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BigCardComponent, MenuBarComponent, MenuTitleComponent, SmallCardComponent, HomeComponent],
+  imports: [RouterOutlet, BigCardComponent, MenuBarComponent, MenuTitleComponent, SmallCardComponent, HomeComponent, ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
