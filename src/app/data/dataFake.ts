c9d41c5993863c2cd7cc3c6d0bfc4e1771ca764a
Cauan Cem <cacocembritosoares@gmail.com>
2024-07-09 15:38:11 -0300

    Dinamizando páginas

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "aaa",
+    "title": "Novo Sandero 2024 chega ao Brasil",
+    "description": "A Renault lançou oficialmente o novo Sandero 2024 no mercado brasileiro. O hatch traz atualizações no design e novos recursos.",
+    "photoCover": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhZsxb2Kn7Br9_5aJ85Fc9QSEuwvFu8wrxzx_O-XkZsmPJ8fIvzNMZr8cQ52_nW7tlWhQyua2HWKvs9mMj37dfobv0kJIqWslYndNiBDqSarPYDw_uNDdnF8eyxlGi16sdRYmldqHQck3I/s1600/Renault-Sandero-2013-branco.jpg"
+  },
+  {
+    "id": "2",
+    "photo": "aaa",
+    "title": "Sandero Stepway 2024 ganha versão aventureira",
+    "description": "A versão aventureira do Sandero Stepway 2024 chega com novos detalhes off-road, suspensão elevada e visual robusto.",
+    "photoCover": ""
+  },
+  {
+    "id": "3",
+    "photo": "aaa",
+    "title": "Sandero 2024: Preços e versões",
+    "description": "Confira os preços e versões do novo Sandero 2024. O hatch está disponível em diferentes configurações para atender a todos os gostos.",
+    "photoCover": ""
+  },
+  {
+    "id": "4",
+    "photo": "aaa",
+    "title": "Sandero 2024: Tecnologia e segurança",
+    "description": "O novo Sandero 2024 traz uma série de recursos tecnológicos e de segurança, como central multimídia, câmera de ré e airbags.",
+    "photoCover": ""
+  }
+];
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { assertInInjectionContext, Component } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
 export class ContentComponent {
+  photoCover:string= ""
+  contentTitle:string = ""
+  contentDescription:string = "Sandero"
+  private id:string | null = "0"
 
+  constructor (
+    private route:ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      this.id = value.get("id"))
+
+      this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id: string | null) {
+    if (id !== null) {
+      const result = dataFake.filter(article => article.id === id);
+      if (result.length > 0) {
+        this.photoCover = result[0].photo;
+        this.contentTitle = result[0].title;
+        this.contentDescription = result[0].description;
+      } else {
+        // Trate o caso em que nenhum elemento foi encontrado
+        console.log('Nenhum elemento encontrado com o ID:', id);
+      }
+    } else {
+      // Trate o caso em que o id é null
+      console.log('ID is null');
+    }
+  }
 }
